d06e72b3a608680c5345400d420ee3312c4e9b14
HM <hieumdd@gmail.com>
2022-09-19 11:04:40 +0700

    refactor: Refactor into sender_action module

--- messenger/repository.py
+++ messenger/repository.py
@@ -4,12 +4,24 @@ import httpx
 
 BASE_URL = "https://graph.facebook.com"
 
-client = httpx.Client(
-    base_url=f"{BASE_URL}/v14.0",
-    params={"access_token": os.getenv("FB_PAGE_ACCESS_TOKEN", "")},
-)
 
-unversioned_client = httpx.Client(
-    base_url=BASE_URL,
-    params={"access_token": os.getenv("FB_PAGE_ACCESS_TOKEN", "")},
-)
+def create_client(base_url: str):
+    return httpx.Client(
+        base_url=base_url,
+        headers={"Content-Type": "application/json"},
+        params={"access_token": os.getenv("FB_PAGE_ACCESS_TOKEN", "")},
+    )
+
+
+client = create_client(f"{BASE_URL}/v14.0")
+
+unversioned_client = create_client(f"{BASE_URL}")
+
+
+def send_message(recipient_id: str, payload: dict) -> dict:
+    payload = {
+        "messaging_type": "RESPONSE",
+        "recipient": {"id": recipient_id},
+        **payload,
+    }
+    return client.post("/", json=payload).json()
