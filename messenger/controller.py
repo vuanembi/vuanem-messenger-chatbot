e204642558c06af238f786793e61e1d7e4948c93
HM <hieumdd@gmail.com>
2022-09-19 14:13:46 +0700

    feat: Add Webhook Controller

--- messenger/controller.py
+++ messenger/controller.py
@@ -1,10 +1,34 @@
+import re
+
 from flask import Blueprint, request
 
 from messenger.verify.service import verify_service
+from messenger.message_response.router import message_response_router
 
-messenger_controller = Blueprint('messenger', __name__)
+messenger_controller = Blueprint("messenger", __name__)
 
-@messenger_controller.get('/')
+
+@messenger_controller.get("/")
 def verify_controller():
     query = request.args
     return verify_service(query)
+
+
+@messenger_controller.post("/")
+def webhook_controller():
+    data = request.get_json(silent=True)
+
+    [handle_entry(entry) for entry in data["entry"]]
+
+    return ("EVENT_RECEIVED", 200)
+
+
+def handle_entry(entry):
+    messaging = entry["messaging"].pop()
+
+    if messaging.get('messages'):
+        handler = message_response_router
+    else:
+        handler = lambda _: True
+
+    return handler(messaging)
--- messenger/message_response/debug/test_service.py
+++ messenger/message_response/debug/test_service.py
@@ -6,10 +6,7 @@ def test_debug_service():
         "sender": {"id": "3707575876035255"},
         "recipient": {"id": "153143011385254"},
         "timestamp": 1663563505357,
-        "message": {
-            "mid": "m_tShq36qYjA8tRXpBE8Mi4BtO3DMoAfweFY82MmuyfeFy8vvDJzHanLjUwo6YGlKgwpC8uQQjb2HM_kVxyRtxSg",
-            "text": "ads",
-        },
+        "message": {"mid": "", "text": "ads"},
     }
     res = service.debug(messaging)
     assert res == True
