3817204ecbddc589a2b8684ce7d53cae3d3cda87
buithuhak22httta <realchanexok@gmail.com>
2022-09-20 18:03:56 +0700

    add_feat_typing_on

--- messenger/sender_action/service.py
+++ messenger/sender_action/service.py
@@ -1,3 +1,3 @@
 from messenger.sender_action.repository import send_sender_action
 
-mark_seen = send_sender_action("mark_seen")
+mark_seen = send_sender_action("typing_on")
--- messenger/sender_action/test_service.py
+++ messenger/sender_action/test_service.py
@@ -2,6 +2,6 @@ import os
 
 from messenger.sender_action import service
 
-def test_mark_seen():
+def test_typing_on():
     res = service.mark_seen(os.getenv('FB_PAGE_SCOPED_ID'))
     assert res
