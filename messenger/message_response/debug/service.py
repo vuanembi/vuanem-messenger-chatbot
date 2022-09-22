a0961421d1f5720e75c2cc2bf8517b8b48bd80b8
Bùi Khánh Huyền <buikhanhhuyena4@gmail.com>
2022-09-22 16:22:10 +0700

    feat: Update debug

--- messenger/message_response/debug/service.py
+++ messenger/message_response/debug/service.py
@@ -1,8 +1,13 @@
 import json
+import re
 
 from messenger.message_response.repository import send_message_response
+from messenger.message_response.echo import echo
 
-
-def debug(messaging):
+def debug_send_message_response(messaging):
     send_message_response(messaging["sender"]["id"], {"text": json.dumps(messaging)})
+    return True
+
+def debug_echo(messaging):
+    echo(messaging)
     return True
--- messenger/message_response/debug/test_service.py
+++ messenger/message_response/debug/test_service.py
@@ -1,12 +1,22 @@
 from messenger.message_response.debug import service
 
 
-def test_debug_service():
+def test_send_message_response_service():
     messaging = {
         "sender": {"id": "3707575876035255"},
         "recipient": {"id": "153143011385254"},
         "timestamp": 1663563505357,
         "message": {"mid": "", "text": "ads"},
     }
-    res = service.debug(messaging)
+    res = service.debug_send_message_response(messaging)
+    assert res == True
+
+def test_echo():    
+    messaging = {
+        "sender": {"id": "3707575876035255"},
+        "recipient": {"id": "153143011385254"},
+        "timestamp": 1663563505357,
+        "message": {"mid": "", "text": "ads"},
+    }
+    res = service.debug_echo(messaging)
     assert res == True
