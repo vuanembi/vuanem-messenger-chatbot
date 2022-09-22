dd279bdba5519d44065484cc455af80ce6f4d687
Bùi Khánh Huyền <buikhanhhuyena4@gmail.com>
2022-09-22 16:24:43 +0700

    feat: Reformat

--- messenger/message_response/debug/service.py
+++ messenger/message_response/debug/service.py
@@ -4,9 +4,12 @@ import re
 from messenger.message_response.repository import send_message_response
 from messenger.message_response.echo import echo
 
+
 def debug_send_message_response(messaging):
-    send_message_response(messaging["sender"]["id"], {"text": json.dumps(messaging)})
+    send_message_response(messaging["sender"]["id"], {
+                          "text": json.dumps(messaging)})
     return True
+
 
 def debug_echo(messaging):
     echo(messaging)
--- messenger/message_response/echo.py
+++ messenger/message_response/echo.py
@@ -1,5 +1,6 @@
 import json
 from message_response.repository import send_message_response
 
+
 def echo(messaging: dict) -> dict:
     return send_message_response(messaging["sender"]["id"], {"text": json.dumps(messaging)})
