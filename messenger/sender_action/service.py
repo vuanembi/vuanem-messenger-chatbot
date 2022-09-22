9b4d38db5a58776259e74fe629c2788301c93f0f
buithuhak22httta <realchanexok@gmail.com>
2022-09-22 09:53:15 +0700

    update feat

--- messenger/sender_action/service.py
+++ messenger/sender_action/service.py
@@ -1,3 +1,4 @@
 from messenger.sender_action.repository import send_sender_action
-
+mark_seen = send_sender_action("mark_seen")
 typing_on = send_sender_action("typing_on")
+typing_off = send_sender_action("typing_off")
--- messenger/sender_action/test_service.py
+++ messenger/sender_action/test_service.py
@@ -2,6 +2,14 @@ import os
 
 from messenger.sender_action import service
 
+def test_mark_seen():
+    res = service.mark_seen(os.getenv('FB_PAGE_SCOPED_ID'))
+    assert res
+
 def test_typing_on():
     res = service.typing_on(os.getenv('FB_PAGE_SCOPED_ID'))
+    assert res
+
+def test_typing_off():
+    res = service.typing_off(os.getenv('FB_PAGE_SCOPED_ID'))
     assert res
