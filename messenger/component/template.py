1518019656da939538ac157a1ed5fb9d2139ae08
HM <hieumdd@gmail.com>
2022-09-30 10:54:59 +0700

    feat: Add product_category & store_location template

--- messenger/component/template.py
+++ messenger/component/template.py
@@ -1,3 +1,50 @@
+def product_category_carousel_attachment():
+    return {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": [
+                {
+                    "title": "Nệm - Đệm",
+                    "image_url": "https://i.imgur.com/hQZYNXA.png",
+                    "subtitle": "Mua Nệm, Đệm giá tốt đến ngay Vua Nệm",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://vuanem.com/nem",
+                    },
+                },
+                {
+                    "title": "Chăn Ga Gối - Drap Giường",
+                    "image_url": "https://i.imgur.com/HgxtI7e.png",
+                    "subtitle": "Chăn Ga Gối Nệm Chính Hãng Cao Cấp",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://vuanem.com/chan-ga-goi",
+                    },
+                },
+                {
+                    "title": "Phụ Kiện",
+                    "image_url": "https://i.imgur.com/afGTY49.png",
+                    "subtitle": "Phụ kiện cao cấp nhập khẩu chính hãng",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://vuanem.com/phu-kien",
+                    },
+                },
+                {
+                    "title": "Giường",
+                    "image_url": "https://i.imgur.com/hQZYNXA.png",
+                    "subtitle": "Mua giường ngủ giá rẻ, cao cấp tại Vua Nệm",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://vuanem.com/giuong",
+                    },
+                },
+            ],
+        },
+    }
+
+
 def mattress_category_carousel_attachment():
     return {
         "type": "template",
@@ -29,6 +76,26 @@ def mattress_category_carousel_attachment():
                     "default_action": {
                         "type": "web_url",
                         "url": "https://vuanem.com/nem/lo-xo",
+                    },
+                },
+            ],
+        },
+    }
+
+
+def store_location_attachment():
+    return {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": [
+                {
+                    "title": "Hệ thống Cửa hàng",
+                    "image_url": "https://i.imgur.com/hQZYNXA.png",
+                    "subtitle": "Hệ thống 152 cửa hàng Chăn ga Gối Nệm",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://vuanem.com/stores",
                     },
                 },
             ],
