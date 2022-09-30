e40f7847ecdd8fee3e4463fdd7540d2da54b47b3
HM <hieumdd@gmail.com>
2022-09-30 16:21:42 +0700

    fix: Fix phone_number capture

--- messenger/component/quick_reply/phone_number/test_phone_number.py
+++ messenger/component/quick_reply/phone_number/test_phone_number.py
@@ -7,7 +7,7 @@ def test_quick_reply_phone_number_handler_success():
     messaging = {
         "sender": {"id": "1254459154682919"},
         "recipient": {"id": "682498171943165"},
-        "timestamp": int(datetime.utcnow().timestamp()),
+        "timestamp": int(datetime.utcnow().timestamp()) * 1000,
         "message": {
             "quick_reply": {"payload": "+84773314403"},
             "mid": "m_AG5Hz2Uq7tuwNEhXfYYKj8mJEM_QPpz5jdCK48PnKAjSdjfipqxqMvK8ma6AC8fplwlqLP_5cgXIbu7I3rBN0P",
